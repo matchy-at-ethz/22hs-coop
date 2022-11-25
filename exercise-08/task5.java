f28cfc0d0a7b485c3b89188f01840614239e1afb
matchy233 <matchy233@gmail.com>
2022-11-25 11:49:46 +0100

    ex8: add detailed explanation task5.java

--- exercise-08/task5.java
+++ exercise-08/task5.java
@@ -1,17 +1,19 @@
-package coop;
 
 import java.util.*;
 
 public class task5 {
     public static String concatenate (List<?> list) {
+    // or maybe   String concatenate (List<?> list, Class<?> c) { ... }
+    // REFLECTIOOOOONNNN
+    // c is an Object that tells you the runtime type info
         String result = "";
         String separator = "";
 
-        if (list instanceof List<String>) {
+        if (list instanceof List<?>) {
             result = "String:";
             separator = " ";
         }
-        else if (list instanceof List<Integer>) {
+        else if (list instanceof List<?>) {
             result = "Integers:";
             separator = "+";
         }
@@ -21,9 +23,15 @@ public class task5 {
         return result;
     }
 
-    public void main (String[] args) {
-        List<String> l = new LinkedList<String>();
-        l.add("foo"); l.add("bar"); l.add("foobar");
-        concatenate(l);
+    public static void main (String[] args) {
+        List<String> l1 = new LinkedList<String>();
+        l1.add("word");
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(1);
+        List<Object> l3 = new LinkedList<Object>();
+        l3.add(new Object());
+        System.out.println(concatenate(l1));
+        System.out.println(concatenate(l2));
+        System.out.println(concatenate(l3));
     }
 }
